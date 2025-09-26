a46bc37ac630e12c9f55ae5dfbfd71fe85221602
kamimefis <camilalobos.2307@gmail.com>
2025-09-26 00:48:46 -0300

    settings trusted_origins

--- nasa_apod_project/settings.py
+++ nasa_apod_project/settings.py
@@ -133,5 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://web-production-791eb.up.railway.app/"
+    "https://web-production-791eb.up.railway.app"
 ]
